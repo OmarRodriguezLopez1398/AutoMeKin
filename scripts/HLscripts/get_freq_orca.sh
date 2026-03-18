6e3f9a3f28d299f7dabe4a16030dd75bf73ea67e
OmarRodriguezLopez1398 <omar.rodriguez.lopez@usc.es>
2026-03-18 12:39:45 +0100

    updating git_freq_orca.sh

--- scripts/HLscripts/get_freq_orca.sh
+++ scripts/HLscripts/get_freq_orca.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# Extrae frecuencias de ORCA (cm-1), una por línea
-# Sustituye frecuencias imaginarias menores de 1 por 1 (igual que get_freq_g09.sh)
+# Vibrational frequencies in cm-1 from ORCA output
+# Line format: "     6:     720.77 cm**-1"
 awk '/VIBRATIONAL FREQUENCIES/{
-        getline; getline   # saltar cabecera
-        while(1){
-            getline
-            if(NF==0) break
-            # línea típica: "  0:       0.00 cm**-1"
-            freq=$2
-            # eliminar el signo negativo para comparar magnitud
-            val=freq; if(val<0) val=-val
-            if(val<1) freq=1
-            printf "%5.0f\n", freq
-        }
-     }' $1
+   getline   # skip "---..." line
+   getline   # skip blank line
+   getline   # skip "Scaling factor..." line
+   getline   # skip blank line
+   while(1){
+      getline
+      if($0 !~ /cm\*\*-1/) break
+      freq=$2
+      val=freq; if(val<0) val=-val
+      if(val<1) freq=1
+      printf "%5.0f\n", freq
+   }
+}' $1
