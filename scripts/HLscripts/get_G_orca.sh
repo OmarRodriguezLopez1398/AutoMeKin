12cbd55137748e146807944159096ee7a5187891
OmarRodriguezLopez1398 <omar.rodriguez.lopez@usc.es>
2026-03-20 14:23:17 +0100

    updating get_G-orca.sh

--- scripts/HLscripts/get_G_orca.sh
+++ scripts/HLscripts/get_G_orca.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Final Gibbs free energy in Hartree from ORCA output - takes LAST instance
-awk '/Final Gibbs free energy/{g=$(NF-1)} END{print g}' $1
+awk '/G-E\(el\)/{g=$(NF-3)} END{print g}' $1
