39cd663f905a78965acc19317ec48ad54be5ff0f
OmarRodriguezLopez1398 <omar.rodriguez.lopez@usc.es>
2026-03-26 12:08:44 +0100

    updating get_NM_orca_molden

--- scripts/HLscripts/get_NM_orca_molden.sh
+++ scripts/HLscripts/get_NM_orca_molden.sh
@@ -10,49 +10,68 @@ echo "       " >> $2.molden
 echo "[FR-COORD]" >> $2.molden
 
 awk 'BEGIN{atobohr=1.889726}
-# Get last geometry in Angstrom and convert to Bohr
+
 /CARTESIAN COORDINATES \(ANGSTROEM\)/{
-   getline   # skip "---..." line
+   getline
    delete sym; delete x; delete y; delete z
-   i=1
+   natom=0
    while(1){
       getline
       if(NF==0) break
-      sym[i]=$1; x[i]=$2; y[i]=$3; z[i]=$4
-      natom=i; i++
+      natom++
+      sym[natom]=$1; x[natom]=$2; y[natom]=$3; z[natom]=$4
    }
 }
-# Get normal modes - ORCA prints them in blocks of 6
+
 /NORMAL MODES/{
-   getline; getline; getline; getline; getline   # skip headers
-   nfreq=0
-   while(1){
-      getline
-      if(NF==0) break
-      if($1~/[0-9]+:/) {
-         # This is a mode index line, read the block
-         ncols=NF-1    # number of modes in this block
-         for(k=1;k<=ncols;k++) modecol[k]=nfreq+k
-         for(at=1;at<=natom*3;at++){
-            getline
-            for(k=1;k<=ncols;k++) nm[modecol[k],at]=$(k+1)
-         }
-         nfreq+=ncols
-      }
-   }
+   for(i=1;i<=5;i++) getline
+   delete tmp_nm
+   cur_nfreq=0; cur_ncols=0
+   in_nm=1
+   next
 }
-END{
-   # Print geometry in Bohr
-   for(i=1;i<=natom;i++){
-      print sym[i], atobohr*x[i], atobohr*y[i], atobohr*z[i] >> "'$2'.molden"
+
+in_nm==1{
+   if(NF==0) next
+
+   is_header=1
+   for(k=1;k<=NF;k++) if($k !~ /^[0-9]+$/) { is_header=0; break }
+
+   if(is_header && NF>=1){
+      cur_nfreq+=cur_ncols
+      cur_ncols=NF
+      for(k=1;k<=cur_ncols;k++) modecol[k]=cur_nfreq+k
+      next
    }
+
+   if($1~/^[0-9]+$/ && NF>1){
+      row=$1+1
+      for(k=1;k<=cur_ncols;k++) tmp_nm[modecol[k],row]=$(k+1)
+      next
+   }
+
+   in_nm=0
+   cur_nfreq+=cur_ncols
+   nfreq=cur_nfreq
+   delete nm
+   for(key in tmp_nm) nm[key]=tmp_nm[key]
+}
+
+END{
+   if(in_nm==1){
+      cur_nfreq+=cur_ncols
+      nfreq=cur_nfreq
+      delete nm
+      for(key in tmp_nm) nm[key]=tmp_nm[key]
+   }
+
+   for(i=1;i<=natom;i++)
+      print sym[i], atobohr*x[i], atobohr*y[i], atobohr*z[i] >> "'$2'.molden"
    print "" >> "'$2'.molden"
    print "[FR-NORM-COORD]" >> "'$2'.molden"
-   # Print normal modes
-   for(i=1;i<=nfreq;i++){
-      print "Vibration "i >> "'$2'.molden"
-      for(j=1;j<=natom;j++){
+   for(i=7;i<=nfreq;i++){
+      print "Vibration "i-6 >> "'$2'.molden"
+      for(j=1;j<=natom;j++)
          print nm[i,3*j-2]*atobohr, nm[i,3*j-1]*atobohr, nm[i,3*j]*atobohr >> "'$2'.molden"
-      }
    }
 }' $1
